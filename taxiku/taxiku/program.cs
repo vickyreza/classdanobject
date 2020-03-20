eac8c7d06e61553b3143f39d166a494b16ac34b5
vickyreza <vicky.fa@students.amikom.ac.id>
2020-03-20 21:03:46 +0700

    inisialisasi project

---
+++ taxiku/taxiku/program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace taraa
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            taxiku taxi = new taxiku();
+
+            taxi.drivername = "dias;";
+            taxi.onduty = true;
+            taxi.numpassenger = 10;
+
+            taxi.taxiinfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+        }
+    }
+}
---
+++ taxiku/taxiku/taxiku.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+namespace taraa
+
+{
+    public class taxiku
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int numpassenger { get; set; }
+
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("nama driver : {0}", drivername);
+            if (onduty == true)
+            {
+                Console.WriteLine("on duty : yes");
+
+            }
+            if (onduty == false)
+            {
+                Console.WriteLine("on duty : no");
+            }
+            Console.WriteLine("number of passenger : {0}", numpassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", drivername);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", drivername);
+        }
+    }
+}
